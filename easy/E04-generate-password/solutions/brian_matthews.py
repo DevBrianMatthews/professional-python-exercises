6794d529c2c72b00451919f6800c75e4249bd8b0
BrianMatthews <brianmatthews.contact@gmail.com>
2026-05-05 19:03:46 -0500

    feat: add E04 password generator

---
+++ easy/E04-generate-password/solutions/brian_matthews.py
@@ -0,0 +1,40 @@
+import random
+import string
+
+def generate_password(length: int = 12, uppercase: bool = True, digits: bool = True, special_chars: bool = True) -> str:
+    lowercase   = string.ascii_lowercase
+    upper       = string.ascii_uppercase
+    nums        = string.digits
+    simbols     = '!@#$%^&*'
+
+    password = []
+    pool     = lowercase
+
+    if length < 8:
+        raise ValueError('length must be at least 8')
+
+    if not uppercase and not digits and not special_chars:
+        raise ValueError('at least one character type must be enabled.')
+
+    if uppercase:
+        password.append(random.choice(upper))
+        pool += upper
+
+    if digits:
+        password.append(random.choice(nums))
+        pool += nums
+
+    if special_chars:
+        password.append(random.choice(simbols))
+        pool += simbols
+
+    while length > len(password):
+        pool_random = random.choice(pool)
+        password.append(pool_random)
+
+    random.shuffle(password)
+    text = ''.join(password)
+
+    return text
+
+print(generate_password(8, True, True, True))
